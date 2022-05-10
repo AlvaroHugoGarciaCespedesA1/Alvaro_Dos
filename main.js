472b93b8948fa8d78bdf568001bad8313fc279db
Alvaro Hugo Garcia Cespedes <alvaro.garcia.c2022@gmail.com>
2022-05-10 00:43:00 -0400

    DC

---
+++ main.js
@@ -0,0 +1,24 @@
+let p1 = document.getElementById('p1')
+let p2 = document.getElementById('p2')
+let p3 = document.getElementById('p3')
+
+
+p1.addEventListener('click', ()=>{
+  window.location.assign("https://alvaro-hugo-garcia-cespedes-1.vercel.app/")
+})
+p2.addEventListener('click', ()=>{
+  window.location.assign("https://alvaro-hugo-garcia-cespedes-2.vercel.app/")
+})
+p3.addEventListener('click', ()=>{
+  window.location.assign("https://alvarohugogarciacespedes.github.io/AlvaroHugoGarciaCespedes_3/")
+})
+
+
+
+
+
+
+
+
+
+
